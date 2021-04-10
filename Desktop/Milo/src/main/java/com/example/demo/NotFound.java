e3f3119a648db9a275dd8eca9a7d6a5807c94697
Hageromo <44583424+Hageromo@users.noreply.github.com>
2021-04-10 17:06:46 +0200

    Delete Desktop directory

--- Desktop/Milo/src/main/java/com/example/demo/NotFound.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class NotFound extends RuntimeException {
-
-    NotFound(Long id){
-        super("Nie znaleziono " + id);
-    }
-}
--- Desktop/Sem5/Aplikacje internetowe/untitled/postac.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Controller;
-use Symfony\Bundle\FtameworkBundle\Controller\AbstractController;
-
-class postac extends AbstractController{
-
-}
